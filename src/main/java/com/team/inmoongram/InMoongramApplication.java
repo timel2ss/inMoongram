836fcccff5a8dda190ea0f7618f3478f0b4ebad8
Junsu <murane@naver.com>
2021-06-26 13:38:46 +0900

    initial commit

---
+++ src/main/java/com/team/inmoongram/InMoongramApplication.java
@@ -0,0 +1,13 @@
+package com.team.inmoongram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InMoongramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InMoongramApplication.class, args);
+    }
+
+}
