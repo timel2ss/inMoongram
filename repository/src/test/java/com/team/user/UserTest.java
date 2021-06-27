808ce9028fc3199c451eb6ea2357a7eb8456d78b
Junsu <murane@naver.com>
2021-06-28 03:04:52 +0900

    test: 유저 팔로우 연관관계 테스트

---
+++ repository/src/test/java/com/team/user/UserTest.java
@@ -0,0 +1,51 @@
+package com.team.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class UserTest {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private FollowRepository followRepository;
+    @Test
+    void userEntityTest(){
+        User user1 = User.builder()
+                .email("murane@naver.com")
+                .introduction("test")
+                .name("정준수")
+                .nickname("스프링")
+                .password("1234")
+                .phoneNumber("010-1111-2222")
+                .profileImage("이미지")
+                .sex(Sex.MALE)
+                .website("www.instagram.com/murane")
+                .build();
+        User user2 = User.builder()
+                .email("asdf12@naver.com")
+                .introduction("test")
+                .name("이용우")
+                .nickname("토비")
+                .password("1234")
+                .phoneNumber("010-1111-2222")
+                .profileImage("이미지")
+                .sex(Sex.MALE)
+                .website("www.instagram.com/murane")
+                .build();
+        userRepository.save(user1);
+        userRepository.save(user2);
+        Follow follow = Follow.builder()
+                .follower(user1)
+                .followee(user2)
+                .build();
+        Follow savedFollow = followRepository.save(follow);
+
+        assertThat(user1).isEqualTo(user2.getFollowers().get(0).getFollower());
+        assertThat(user2).isEqualTo(user1.getFollowees().get(0).getFollowee());
+
+    }
+}
