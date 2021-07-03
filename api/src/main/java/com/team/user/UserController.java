8122ea91e0fe35689dcde8e64a311407e78056fc
peachberry <peachberry0318@gmail.com>
2021-07-03 10:31:31 +0900

    refactor : 필요없는 valid 로직 제거

--- api/src/main/java/com/team/user/UserController.java
+++ api/src/main/java/com/team/user/UserController.java
@@ -2,7 +2,6 @@ package com.team.user;
 
 import com.team.user.dto.command.FollowerInfoListCommand;
 import com.team.user.dto.response.FollowerInfoListResponse;
-import com.team.user.dto.response.FollowerInfoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/user")
@@ -21,7 +18,7 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("follower/list")
-    public ResponseEntity<FollowerInfoListResponse> getFollowerList(@Valid @RequestParam(name = "user-id") Long userId) {
+    public ResponseEntity<FollowerInfoListResponse> getFollowerList(@RequestParam(name = "user-id") Long userId) {
         return ResponseEntity.ok(
                 new FollowerInfoListResponse(
                         userService.getFollowerList(new FollowerInfoListCommand(userId))
