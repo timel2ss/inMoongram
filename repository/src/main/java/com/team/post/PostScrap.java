1783561fb88d5827ac3334ba85d459e5b70500bc
peachberry <peachberry0318@gmail.com>
2021-07-05 19:46:43 +0900

    feat(PostScrap) : 생성자와 양방향 관계 설정 메서드 추가

--- repository/src/main/java/com/team/post/PostScrap.java
+++ repository/src/main/java/com/team/post/PostScrap.java
@@ -23,4 +23,14 @@ public class PostScrap {
     @JoinColumn(name = "post_id")
     @ManyToOne(fetch = FetchType.LAZY)
     private Post post;
+
+    public PostScrap(User user, Post post) {
+        this.user = user;
+        this.post = post;
+        setPost(user, post);
+    }
+
+    private void setPost(User user, Post post) {
+        user.getPostScraps().add(post);
+    }
 }
