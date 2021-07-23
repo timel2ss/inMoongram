1b3373b1b9a7d5c9049c916b0728102af827634c
peachberry <peachberry0318@gmail.com>
2021-07-23 23:33:31 +0900

    refactor : import 삭제

--- service/src/main/java/com/team/post/PostService.java
+++ service/src/main/java/com/team/post/PostService.java
@@ -14,7 +14,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
